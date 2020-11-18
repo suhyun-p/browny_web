c4387cb091b6ff470b30eb556f2165da02761905
suhpark <laztwo@hanmail.net>
2020-11-18 23:38:54 +0900

    classList Rendering

--- src/components/Album.js
+++ src/components/Album.js
@@ -1,25 +1,68 @@
 import React from 'react';
 
-class Album extends React.Component {
-    render() {
-        return (
-            <div class="col-md-4">
-                <div class="card mb-4 shadow-sm">
+const Album= ({classInfo}) => {
+
+    const Genre = (genre) => {
+        switch(genre) {
+            case "B":
+                return "Bachata";
+            case "S":
+                return "Salsa";
+            default:
+                return "";
+        }
+    }
+
+    const Region = (region) => {
+        switch(region) {
+            case "HD":
+                return "홍대";
+            case "GN":
+                return "강남";
+            default:
+                return "";
+        }
+    };
+
+    const ClassType = (type) => {
+        switch(type) {
+            case "O":
+                return "Open";
+            default:
+                return ""
+        }
+    };
+
+    return (
+        <div class="col-md-4">
+            <div class="card mb-4 shadow-sm">
                 <svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: Thumbnail"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"/><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>
                 <div class="card-body">
-                    <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                    <div class="d-flex justify-content-between align-items-center">
-                    <div class="btn-group">
-                        <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
-                        <button type="button" class="btn btn-sm btn-outline-secondary">Edit</button>
+                    <strong class="d-inline-block mb-2 text-primary">{Genre(classInfo.genre)}</strong>
+                    &nbsp;&nbsp;
+                    <strong class="d-inline-block mb-2 text-info">{Region(classInfo.region)}</strong>
+                    &nbsp;&nbsp;
+                    <strong class="d-inline-block mb-2 text-success">{ClassType(classInfo.type)}</strong>
+                    &nbsp;&nbsp;
+                    <strong class="d-inline-block mb-2 text-warning">World</strong>
+                    <h3 class="mb-0">{classInfo.title}</h3>
+                    <div class="mb-1 text-muted">
+                        <span>{classInfo.startDate}</span>
+                        &nbsp;~&nbsp;
+                        <span>{classInfo.endDate}</span>
+                        &nbsp;({classInfo.dateDesc})
+                        <br/>
+                        <span>{classInfo.startTime}</span>
+                        &nbsp;~&nbsp;
+                        <span>{classInfo.endTime}</span>
+                        &nbsp;({classInfo.timeDesc})
                     </div>
-                    <small class="text-muted">9 mins</small>
-                    </div>
-                </div>
+                    <p class="card-text mb-auto">₩{classInfo.price}&nbsp;({classInfo.priceDesc})</p>
+                    {/* <a href="#" class="stretched-link">자세히 보기</a> */}
                 </div>
             </div>
-        )
-    }
+        </div>
+    )
 }
 
 export default Album;
--- src/components/ClassList.js
+++ src/components/ClassList.js
@@ -3,62 +3,142 @@ import Album from './Album.js';
 
 class ClassList extends React.Component {
     state = {
-      isLoading: false,
       classList: []
     };
 
     componentDidMount() {
       const classList = [
         {
-          id: 1
+          seq: 1,
+          genre: "S",
+          region: "HD",
+          title: "아오라 살사 스페셜 에센스 시즌 2",
+          startDate: "11/7",
+          endDate: "12/12",
+          dateDesc: "총 6주, 매주 토요일",
+          startTime: "20:00",
+          endTime: "21:00",
+          timeDesc: "1회 60분",
+          price: "100,000",
+          priceDesc: ""
         },
         {
-          id: 2
+          seq: 2,
+          genre: "B",
+          region: "HD",
+          type: "O",
+          title: "갬성끌올, 트렌디패턴 특강",
+          startDate: "11/6",
+          endDate: "11/27",
+          dateDesc: "총 4주, 매주 금요일",
+          startTime: "20:00",
+          endTime: "21:00",
+          timeDesc: "1회 60분",
+          price: "10,000",
+          priceDesc: "입장료 별도"
         },
         {
-          id: 3
+          seq: 3,
+          genre: "B",
+          region: "GN",
+          type: "",
+          title: "연말빠뤼 발표반",
+          startDate: "11/5",
+          endDate: "12/10",
+          dateDesc: "총 6주, 매주 목요일",
+          startTime: "20:30",
+          endTime: "22:00",
+          timeDesc: "1회 90분",
+          price: "150,000",
+          priceDesc: ""
         },
         {
-          id: 4
+          seq: 4,
+          genre: "B",
+          region: "HD",
+          type: "",
+          title: "연말빠뤼 발표반",
+          startDate: "11/7",
+          endDate: "12/12",
+          dateDesc: "총 6주, 매주 토요일",
+          startTime: "18:00",
+          endTime: "19:30",
+          timeDesc: "1회 90분",
+          price: "150,000",
+          priceDesc: ""
         },
         {
-          id: 5
+          seq: 5,
+          genre: "B",
+          region: "GN",
+          type: "",
+          title: "소수정예 바차타센슈얼 집중코칭반",
+          startDate: "11/5",
+          endDate: "11/26",
+          dateDesc: "총 4주, 매주 목요일",
+          startTime: "19:00",
+          endTime: "20:30",
+          timeDesc: "1회 60분",
+          price: "100,000",
+          priceDesc: ""
         },
         {
-          id: 6
+          seq: 6,
+          genre: "S",
+          region: "HD",
+          type: "",
+          title: "아오라 살사 정규시작반 5기",
+          startDate: "11/7",
+          endDate: "12/12",
+          dateDesc: "총 6주, 매주 토요일",
+          startTime: "20:00",
+          endTime: "21:00",
+          timeDesc: "1회 60분",
+          price: "70,000",
+          priceDesc: ""
         },
         {
-          id: 7
+          seq: 7,
+          genre: "S",
+          region: "HD",
+          type: "",
+          title: "아오라 살사 LV.2",
+          startDate: "11/7",
+          endDate: "12/12",
+          dateDesc: "총 6주, 매주 토요일",
+          startTime: "19:00",
+          endTime: "20:00",
+          timeDesc: "1회 60분",
+          price: "90,000",
+          priceDesc: ""
         },
         {
-          id: 8
-        },
-        {
-          id: 9
-        },
-        {
-          id: 10
-        },
-        {
-          id: 11
-        },
-        {
-          id: 12
+          seq: 8,
+          genre: "B",
+          region: "HD",
+          type: "",
+          title: "아오라 바차타 LV.2",
+          startDate: "11/7",
+          endDate: "12/12",
+          dateDesc: "총 6주, 매주 토요일",
+          startTime: "20:00",
+          endTime: "21:00",
+          timeDesc: "1회 60분",
+          price: "90,000",
+          priceDesc: ""
         }
       ]
-      this.setState({isLoading: true, classList});
+      this.setState({classList});
     };
 
     render() {
-        const { isLoading, classList } = this.state;
+        const { classList } = this.state;
 
         return (
             <div class="album py-5 bg-light">
             <div class="container">
               <div class="row">
-                {!isLoading ? "Loading... " : classList.map((class_) => {
-                  return (<Album key={class_.id}/>)
-                })}
+                {classList.map((class_) => { return <Album key={class_.seq} classInfo={class_} /> })}
               </div>
             </div>
           </div>
